cae9eaf74745b62f332283a034f5036f2f50e735
Ansh <ansh1999arora@gmail.com>
2022-05-20 11:41:04 +0200

    i messed up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout/Layout';
+import { AnimatePresence } from 'framer-motion';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AnimatePresence exitBeforeEnter>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AnimatePresence>
   );
 }
 
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -8,6 +8,8 @@ import Link from 'next/link';
 import styles from '@/styles/ProjectsPage.module.css';
 import Project from '@/components/Project/Project';
 
+import { motion } from 'framer-motion';
+
 interface ProjectsPageProps {
   id: string;
   name: string;
@@ -23,46 +25,52 @@ interface ProjectsPageProps {
 
 const ProjectsPage: NextPage<{ data: ProjectsPageProps[] }> = ({ data }) => {
   return (
-    <div className={styles.ProjectsPage}>
-      <Head>
-        <title>Projects</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <motion.div
+      initial={{ x: '100vw', opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: '100vh', opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className={styles.ProjectsPage}>
+        <Head>
+          <title>Projects</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <Link href="/">
-        <a>
-          <img className={styles.back} alt="back" src="/imgs/back.svg"></img>
-        </a>
-      </Link>
+        <Link href="/">
+          <a>
+            <img className={styles.back} alt="back" src="/imgs/back.svg"></img>
+          </a>
+        </Link>
 
-      <div className={styles.container}>
-        <div className={styles.project_links}>
-          <p>Project 1</p>
-          <p>Project 2</p>
-          <p>Project 3</p>
-          {/* <p>Project 4</p> */}
-        </div>
-        <div className={styles.left}>
-          <h1 className={styles.heading}>
-            Recent <br /> Projects
-          </h1>
-        </div>
-        <div className={styles.right}>
-          <div className={styles.image_wrapper}>
-            <img src="/imgs/Enthusiastic.gif" alt="projects" />
+        <div className={styles.container}>
+          {/* <div className={styles.project_links}>
+            <p>Project 1</p>
+            <p>Project 2</p>
+            <p>Project 3</p>
+          </div> */}
+          <div className={styles.left}>
+            <h1 className={styles.heading}>
+              Recent <br /> Projects
+            </h1>
+          </div>
+          <div className={styles.right}>
+            <div className={styles.image_wrapper}>
+              <img src="/imgs/Enthusiastic.gif" alt="projects" />
+            </div>
           </div>
         </div>
+        {data.map((project) => (
+          <Project
+            key={project.id}
+            name={project.name}
+            shortDescription={project.shortDescription}
+            description={project.description.text}
+            image={project.image.url}
+          />
+        ))}
       </div>
-      {data.map((project) => (
-        <Project
-          key={project.id}
-          name={project.name}
-          shortDescription={project.shortDescription}
-          description={project.description.text}
-          image={project.image.url}
-        />
-      ))}
-    </div>
+    </motion.div>
   );
 };
 
